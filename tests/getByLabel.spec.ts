a70b4fd684d4dc7e7810f969e5ae558c7659e373
AkarshitMahajan2 <akarshitmahajan156@gmail.com>
2026-02-22 16:58:34 +0530

    done 5 tests

--- tests/getByLabel.spec.ts
+++ tests/getByLabel.spec.ts
@@ -1,7 +1,8 @@
 import{test,expect} from '@playwright/test';
+const url='https://testautomationpractice.blogspot.com/p/playwrightpractice.html';
 
 test('Inputing Values test', async ({ page }) => {
-    await page.goto('https://testautomationpractice.blogspot.com/p/playwrightpractice.html');
+    await page.goto(url);
     const adr=await page.getByLabel('Email Address:');
     adr.focus();
     await adr.fill('Aperion123@gmail.com');
@@ -29,7 +30,7 @@ test('Inputing Values test', async ({ page }) => {
 
 
 test('Shiiping method test', async ({ page }) => {
-    await page.goto('https://testautomationpractice.blogspot.com/p/playwrightpractice.html');
+    await page.goto(url);
     const legend= page.getByText('Shipping Method');
     await expect(legend).toBeVisible();
     await expect(legend).toHaveText('Shipping Method');
@@ -48,4 +49,47 @@ test('Shiiping method test', async ({ page }) => {
     await expect(stshiping).not.toBeChecked();
 
 
+  });
+
+  test('enter email & password and then erase email and check the value', async ({ page }) => {
+    await page.goto(url);
+    const adr= page.getByLabel('Email Address:');
+    adr.focus();
+    await adr.fill('Aperion123@gmail.com');
+    await expect(adr).toHaveValue('Aperion123@gmail.com');
+    const pass= page.getByLabel(' Password: ');
+    pass.focus();
+    await pass.fill('123456789');
+    await expect(pass).toHaveValue('123456789');
+    await adr.clear();
+    await expect(adr).toHaveValue('');
+  });
+
+  test('enter age and then check the value and then enter value and check the value', async ({ page }) => {
+    await page.goto(url);
+    const ag= page.getByLabel('Age:');
+    ag.focus();
+    await ag.fill('19');
+    await expect(ag).toHaveValue('19');
+    await ag.clear();
+    await expect(ag).toHaveValue('');
+    await ag.fill('20');
+    await expect(ag).toHaveValue('20');
+
+  });
+
+  test('fill email and click on shipping method and check the email value & clear & fill new value & check clicbox ', async ({ page }) => {
+    await page.goto(url);
+    const adr= page.getByLabel('Email Address:');
+    adr.focus();
+    await adr.fill('Aperion123@gmail.com');
+    await expect(adr).toHaveValue('Aperion123@gmail.com');
+    const stshiping=page.getByLabel(' Standard');
+    await stshiping.check();
+    await expect(stshiping).toBeChecked();
+    await adr.clear();
+    await expect(adr).toHaveValue('');
+    await adr.fill('NewEmail@gmail.com');
+    await expect(adr).toHaveValue('NewEmail@gmail.com');
+    await expect(stshiping).toBeChecked();
   });
