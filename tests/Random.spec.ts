a737d0cc85cbffdfd9ba8448915a6c918ad156d9
AkarshitMahajan2 <akarshitmahajan156@gmail.com>
2026-02-22 19:19:13 +0530

    added MouseHover &' Drag and Drop Test'

--- tests/Random.spec.ts
+++ tests/Random.spec.ts
@@ -1,5 +1,5 @@
 import{test,expect} from '@playwright/test';
-const url='https://testautomationpractice.blogspot.com/p/playwrightpractice.html';
+const url='https://testautomationpractice.blogspot.com/p/playwrightpractice.html#';
 
 test('Wikepedia Test', async ({page}) => {
     await page.goto(url);
@@ -26,4 +26,30 @@ test('Wikepedia Test', async ({page}) => {
     // await btnele.click();
 
 
+});
+
+//  Drag and Drop Test
+
+test('Drag and Drop Test', async ({page}) => {
+    await page.goto(url);
+    // css using 
+    const source=page.locator('#draggable');
+    const target=page.locator('#droppable');
+    await source.dragTo(target);
+    await page.waitForTimeout(4000);
+});
+
+// Mouse Hover Test
+
+test('Mouse Hover Test', async ({page}) => {
+    await page.goto(url);
+    await page.getByText('Point Me').hover();
+    await page.waitForTimeout(4000);
+    await page.getByRole('link', { name: 'Mobiles' }).click();
+    await expect(page).toHaveURL(url);
+    // second laptop test
+    await page.getByText('Point Me').hover();
+    await page.waitForTimeout(4000);
+    await page.getByRole('link', { name: 'Laptops' }).click();
+    await expect(page).toHaveURL(url);
 });
