b6ebdcfebcd95b35085d30598fab4825688eaad6
AkarshitMahajan2 <akarshitmahajan156@gmail.com>
2026-02-22 12:33:13 +0530

    added 1 test

--- tests/getByRole.spec.ts
+++ tests/getByRole.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect, Locator } from '@playwright/test';
+const url='https://testautomationpractice.blogspot.com/p/playwrightpractice.html';
 
 test('Button test', async ({ page }) => {
-  await page.goto('https://testautomationpractice.blogspot.com/p/playwrightpractice.html');
+  await page.goto(url);
 
 
 const button = await page.getByRole('button', { name: 'Primary Action' });
@@ -16,7 +17,7 @@ await button3.click();
 
 
 test("Headings test", async ({ page }) => {
-    await page.goto('https://testautomationpractice.blogspot.com/p/playwrightpractice.html');
+    await page.goto(url);
      await expect(page.getByRole('heading', { name: 'Buttons' })).toBeVisible();
      await expect(page.getByRole('heading', { name: 'Form Elements' })).toBeVisible();
      await expect(page.getByRole('heading', { name: 'Navigation' })).toBeVisible();
@@ -24,7 +25,7 @@ test("Headings test", async ({ page }) => {
 
 
 test("Links test", async ({ page }) => {
-    await page.goto('https://testautomationpractice.blogspot.com/p/playwrightpractice.html');
+    await page.goto(url);
     
     await page.getByRole('menuitem', { name: 'Home' }).click();
     await expect(page).toHaveURL(/testautomationpractice\.blogspot\.com/);
@@ -36,10 +37,23 @@ test("Links test", async ({ page }) => {
 }); 
 
 test("Username test & checkbox", async ({ page }) => {
-    await page.goto('https://testautomationpractice.blogspot.com/p/playwrightpractice.html');
+    await page.goto(url);
     await page.locator('#username').fill('Akarshit');
     await expect(page.locator('#username')).toHaveValue('Akarshit');
     const pagecheck=await expect(page.getByRole('checkbox', { name: 'Accept Terms' })).toBeChecked;
     // await expect(page.getByRole('checkbox', { name: 'Accept Terms' })).toBeChecked();
    console.log(pagecheck);
+   await page.locator('#username').clear();
+});
+
+test("click button afect textfeild or not", async ({ page }) => {
+    await page.goto(url);
+    await page.locator('#username').fill('Akarshit');
+    const button =  page.getByRole('button', { name: 'Primary Action' });
+    await button.click();
+
+    const button2= await page.getByRole('button',{ name: 'Toggle Button' });
+    await button2.click();
+
+    await expect(page.locator('#username')).toHaveValue('Akarshit');
 });
