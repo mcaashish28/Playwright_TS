0a2a6860de4bf01fcff37596d23f5871c8252c62
AkarshitMahajan2 <akarshitmahajan156@gmail.com>
2026-02-26 06:36:22 +0530

    Optimizes code for all courses and links

--- tests/Random2.spec.ts
+++ tests/Random2.spec.ts
@@ -3,214 +3,91 @@ import{test,expect,Page} from '@playwright/test';
 const url='https://www.pavanonlinetrainings.com/p/udemy-courses.html';
 
 // Function for Text Verifying Is it present
-async function nameTesting(page:Page, loc:string,num:number,tag:string  ) {
-      const imgurl= page.locator(`.${loc}`).nth(num);
-      console.log(await imgurl.locator(tag).allInnerTexts());
-   
-     
+/*
+function chnges the prefix by 7 th method class or id
+function do iterating and result come
+
+this function takes the parameter locate the text and then print the text on console and befor printing it verifies also
+that it is present or not then it takes
+
+*/
+async function nameTesting(page:Page, loc:string,num:number,tag:string ,i:number,howmany:number,cla:string) {
+    // "for chnging loactors path by class or id"
+    let prefix='';
+     if(cla=='class'){
+    prefix='.';
+   }else{
+    prefix='#';
+   }
+//   for iteration we know index starts from 0 but not a random persom
+   if(i>0){ i=i-1; }
+//    code iterating 
+    for(i=i;i<howmany;i++){
+        
+      const imgurl= page.locator(`${prefix}${loc}`).nth(num).locator(tag);
+      await imgurl.isVisible();
+      console.log(await imgurl.allInnerTexts());
+      num++;
+        
+
+    }
 }
+     
+
 
 // Function for getting button click and redirected to another site / Link testing 
-async function LinkTesting(page:Page, loc:string,num:number,text:string){
+/*
+   this function takes parameter and then by clicking the link it moves to another tab and then it 
+   locate the page url and print it and wait there for 5 sec and then page closes  
+
+
+*/
+async function LinkTesting(page:Page, loc:string,num:number,text:string,i:number,howmany:number){
+//   for iteration we know index starts from 0 but not a random persom
+    if(i>0){ i=i-1; }
+//    code iterating 
+    for(i=i;i<howmany;i++){
     const btn=page.locator(`.${loc}`).nth(num);
     const [newPage] = await Promise.all([
         page.context().waitForEvent('page'),
         await btn.getByText(text).click()
     ]);
-    console.log(newPage.url());
-    // await btn.getByText(text).click(); 
-        
+        console.log(newPage.url()); 
+        await newPage.waitForTimeout(500);
+        newPage.close();
+}
 }
 
-test('Course0', async ({page}) => {
+
+
+/* nameTesting(page,loc,nthcourse,tag,startingindex,closingindex,{class/id})
+   write nth course and if you want specific course test then you have to write starting index=1,closingindex=1,
+   if and tell class id also in 7 th parameter .
+   
+   LinkTesting(page,loc,nthcourse,tag,startingindex,closingindex)
+   only starting and closing index change,if you want specific test then you chnge only nthcourse  ,starting index=1,closingindex=1 these remains same.  
+*/
+
+
+test('Courses', async ({page}) => {
     await page.goto(url);
-    await nameTesting(page,'course',0,'h2');
-    await LinkTesting(page,'course',0,'View Course');
+    await nameTesting(page,'course',0,'h2',1,1,'class');
+    await LinkTesting(page,'course',0,'View Course',1,1);
    
 
 });
-
-test('Course1', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',1,'h2');
-    await LinkTesting(page,'course',1,'View Course');
-   
-
-});
-
-test('Course2', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',2,'h2');
-    await LinkTesting(page,'course',2,'View Course');
-
-
-});
-
-test('Course3', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',3,'h2');
-    await LinkTesting(page,'course',3,'View Course');
-
-});
-
-test('Course4', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',4,'h2');
-    await LinkTesting(page,'course',4,'View Course');
-
-});
-
-test('Course5', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',5,'h2');
-    await LinkTesting(page,'course',5,'View Course');
-
-});
-
-test('Course6', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',6,'h2');
-    await LinkTesting(page,'course',6,'View Course');
-
-});
-
-
-test('Course7', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',7,'h2');
-    await LinkTesting(page,'course',7,'View Course');
-
-});
-
-
-test('Course8', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',8,'h2');
-    await LinkTesting(page,'course',8,'View Course');
-
-});
-
-
-test('Course9', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',9,'h2');
-    await LinkTesting(page,'course',9,'View Course');
-
-});
-
-
-test('Course10', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',10,'h2');
-    await LinkTesting(page,'course',10,'View Course');
-
-});
-
-
-test('Course11', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',11,'h2');
-    await LinkTesting(page,'course',11,'View Course');
-
-});
-
-
-test('Course12', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',12,'h2');
-    await LinkTesting(page,'course',12,'View Course');
-
-});
-
-
-test('Course13', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',13,'h2');
-    await LinkTesting(page,'course',13,'View Course');
-
-});
-
-
-test('Course14', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',14,'h2');
-    await LinkTesting(page,'course',14,'View Course');
-
-});
-
-
-test('Course15', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',15,'h2');
-    await LinkTesting(page,'course',15,'View Course');
-
-});
-
-
-test('Course16', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',16,'h2');
-    await LinkTesting(page,'course',16,'View Course');
-   
-
-});
-
-
-test('Course17', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',17,'h2');
-    await LinkTesting(page,'course',17,'View Course');
-
-});
-
-
-test('Course18', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',18,'h2');
-    await LinkTesting(page,'course',18,'View Course');
-
-});
-
-
-test('Course19', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',19,'h2');
-    await LinkTesting(page,'course',19,'View Course');
-   
-
-});
-
-
-test('Course20', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',20,'h2');
-    await LinkTesting(page,'course',20,'View Course');
-   
-
-});
-
-
-test('Course21', async ({page}) => {
-    await page.goto(url);
-    await nameTesting(page,'course',21,'h2');
-    await LinkTesting(page,'course',21,'View Course');
-   
-
-});
-
 
 
 test('Heading Test', async ({page}) => {
     await page.goto(url);
-    await nameTesting(page,'description',0,'span');
+    await nameTesting(page,'description',0,'span',1,1,'class');
 
 
 });
 
-
 test('Scrolling Heading Test', async ({page}) => {
     await page.goto(url);
-     await nameTesting(page,'widget-content',0,'b');
+     await nameTesting(page,'HTML3',0,'b',1,1,'id');
 
-     
+   
 });
