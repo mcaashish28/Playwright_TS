4dd5b6133dd7fe3587d87c4e6dd95d4c6bcfb7f9
AkarshitMahajan2 <akarshitmahajan156@gmail.com>
2026-02-26 19:42:04 +0530

    Optimizes Functions also For better results

--- tests/Random2.spec.ts
+++ tests/Random2.spec.ts
@@ -2,77 +2,72 @@ import{test,expect,Page} from '@playwright/test';
 
 const url='https://www.pavanonlinetrainings.com/p/udemy-courses.html';
 
-// Function for Text Verifying Is it present
-/*
-function chnges the prefix by 7 th method class or id
-function do iterating and result come
 
-this function takes the parameter locate the text and then print the text on console and befor printing it verifies also
-that it is present or not then it takes
-
-*/
-async function nameTesting(page:Page, loc:string,num:number,tag:string ,i:number,howmany:number,cla:string) {
-    // "for chnging loactors path by class or id"
+async function nameTesting(page:Page, loc:string,num:number,tag:string ,i:number,howmany:number,cla:string,testing:string) {
     let prefix='';
      if(cla=='class'){
     prefix='.';
    }else{
     prefix='#';
    }
-//   for iteration we know index starts from 0 but not a random persom
+
    if(i>0){ i=i-1; }
-//    code iterating 
-    for(i=i;i<howmany;i++){
+
+   if(testing == 'Courses') {
+    for(i = i;  i < howmany;  i++)  {
         
       const imgurl= page.locator(`${prefix}${loc}`).nth(num).locator(tag);
       await imgurl.isVisible();
       console.log(await imgurl.allInnerTexts());
+      await LinkTesting(page,'course',0,'View Course');
+      await Imagetesting(page,'course img',0,'src','class')
       num++;
-        
 
+        }
+    }else{
+    const imgurl= page.locator(`${prefix}${loc}`).nth(num).locator(tag);
+    await imgurl.isVisible();
+    console.log(await imgurl.allInnerTexts());
     }
 }
      
 
 
-// Function for getting button click and redirected to another site / Link testing 
-/*
-   this function takes parameter and then by clicking the link it moves to another tab and then it 
-   locate the page url and print it and wait there for 5 sec and then page closes  
 
-
-*/
-async function LinkTesting(page:Page, loc:string,num:number,text:string,i:number,howmany:number){
-//   for iteration we know index starts from 0 but not a random persom
-    if(i>0){ i=i-1; }
-//    code iterating 
-    for(i=i;i<howmany;i++){
+async function LinkTesting(page:Page, loc:string,num:number,text:string,){
+  
     const btn=page.locator(`.${loc}`).nth(num);
+
     const [newPage] = await Promise.all([
         page.context().waitForEvent('page'),
         await btn.getByText(text).click()
     ]);
         console.log(newPage.url()); 
-        await newPage.waitForTimeout(500);
         newPage.close();
 }
-}
 
+async function Imagetesting(page:Page, loc:string,num:number,tag:string ,cla:string){
+    let prefix='';
+     if(cla=='class'){
+    prefix='.';
+   }else{
+    prefix='#';
+   }
 
+        
+      const imagelink= page.locator(`${prefix}${loc}`).nth(num);
+      await imagelink.isVisible();
+      console.log(await imagelink.getAttribute(tag));
+      await imagelink.click();
+      await expect(page).toHaveURL('https://www.pavanonlinetrainings.com/p/udemy-courses.html');
+      num++;
+        }
 
-/* nameTesting(page,loc,nthcourse,tag,startingindex,closingindex,{class/id})
-   write nth course and if you want specific course test then you have to write starting index=1,closingindex=1,
-   if and tell class id also in 7 th parameter .
-   
-   LinkTesting(page,loc,nthcourse,tag,startingindex,closingindex)
-   only starting and closing index change,if you want specific test then you chnge only nthcourse  ,starting index=1,closingindex=1 these remains same.  
-*/
 
 
 test('Courses', async ({page}) => {
     await page.goto(url);
-    await nameTesting(page,'course',0,'h2',1,1,'class');
-    await LinkTesting(page,'course',0,'View Course',1,1);
+    await nameTesting(page,'course',0,'h2',1,20,'class','Courses');
    
 
 });
@@ -80,14 +75,13 @@ test('Courses', async ({page}) => {
 
 test('Heading Test', async ({page}) => {
     await page.goto(url);
-    await nameTesting(page,'description',0,'span',1,1,'class');
+    await nameTesting(page,'description',0,'span',1,1,'class','NM');
 
 
 });
 
 test('Scrolling Heading Test', async ({page}) => {
     await page.goto(url);
-     await nameTesting(page,'HTML3',0,'b',1,1,'id');
+     await nameTesting(page,'HTML3',0,'b',1,1,'id','Heading');
 
-   
-});
+   });
