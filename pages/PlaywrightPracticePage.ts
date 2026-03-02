bb7da65c653c5077d5f2a39890e069cabb756565
AkarshitMahajan2 <akarshitmahajan156@gmail.com>
2026-03-02 08:08:09 +0530

    Added Some chnges

--- pages/PlaywrightPracticePage.ts
+++ pages/PlaywrightPracticePage.ts
@@ -13,11 +13,6 @@ export class PlaywrightPracticePage extends BasePage{
        const btn2= await this.getConstantCard(locclass,'link',text);
        await this.ClickandHandleNewPage(btn2);
     }
-
-    async LaptopLinks(locclass:string,text:string){
-       const btn2= await this.getConstantCard(locclass,'heading',text);
-       await this.ClickandHandleNewPage(btn2);
-    }
    
     async PopupChecker(popupname:string){
      await this.PopupAllower(popupname)
@@ -62,5 +57,77 @@ export class PlaywrightPracticePage extends BasePage{
       const btn2=(await this.TestIdclick(pprice)).nth(0);
       await btn1.isVisible();
       await btn2.isVisible();
+    }
+
+    async AltImageIsVisible(text:string): Promise<void>{
+        const img=await this.ALTclick(text);
+        await img.isVisible();
+    }
+
+    async AltImageLoadStatus(text:string){
+        const img=await this.ALTclick(text);
+        await this.CourseImageLoadStatus(img);
+    }
+
+    async AltImagePixelTest(text:string){
+        const img=await this.ALTclick(text);
+        await this.ImagePixelTest(img);
+    }
+
+     async AltImageSizeStatus(text:string){
+        const img=await this.ALTclick(text);
+        const res=await this.CourseImageLoadStatus(img);
+        await this.ImageFileSizeTest(res);
+    }
+
+    async RoleToogleButton(text:string){
+        const btn=await this.Roleclick('button',text);
+        await this.ClickingMethod(btn);
+    }
+
+    async RoleCheckingButton(text:string){
+        const btn=await this.Roleclick('checkbox',text);
+        await this.CheckingMethod(btn);
+    }
+
+    async RoleFillingValues(text:string,value:string){
+        const wait=await this.Roleclick('textbox',text);
+        await this.InputValue(wait,value);
+    }
+
+    async TextVisibilityTest(text:string){
+        const loc=await this.Textclick(text);
+    }
+
+    async TextButton(text:string){
+    const btn=await this.Textclick(text);
+    await this.ClickingMethod(btn);
+    }
+
+    async TitleVisible(text:string){
+        const btn=await this.TITLEclick(text);
+        await btn.isVisible();
+        }
+
+    async TitleButtonClick(text:string){
+        const btn=await this.TITLEclick(text);
+        await this.ClickingMethod(btn);
+    }
+
+    async TitleWorkingButtonClick(text:string){
+        const btn=await this.TITLEclick(text);
+        await this.ClickingMethod(btn);
+    }
+
+    async DragANDDrop(text:string,text2:string){
+        const source=await this.LocatorClick(text);
+        const target=await this.LocatorClick(text2);
+        await source.dragTo(target);
+    }
+
+    async MOUSEHOVER(text1:string,text2:string){
+    const h=await this.Textclick(text1);
+    await this.page.waitForTimeout(300);
+    const btn=await this.Roleclick('link',text2);
     }
 }
